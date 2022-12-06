988ca66ca2572fa10078595c418500c231e568d5
Julianne Swinoga <julianneswinoga@gmail.com>
2022-12-06 14:10:22 -0500

    Add JSBSim integration test (only for FDMv24 for now)

---
+++ tests/jsbsim_wrapper/jsbsim_wrapper.py
@@ -0,0 +1,200 @@
+import math
+import tempfile
+from typing import List, NamedTuple
+
+import jsbsim
+
+from tests.testing_common import fill_xml_template, m_to_ft
+
+
+class Waypoint(NamedTuple):
+    lat_deg: float
+    lon_deg: float
+    alt_m: float  # relative to sea-level
+
+
+class FlightGearUdpOutput(NamedTuple):
+    ip_addr: str
+    port: int
+    rate_hz: int
+    fg_version: int = 24
+
+
+class JsbConfig(NamedTuple):
+    # required
+    waypoints: List[Waypoint]
+    # Must be at least 1 FlightGear output (it'll be the internal one to the python script)
+    flightgear_outputs: List[FlightGearUdpOutput]
+    # optional
+    vehicle_str: str = 'c310'
+    wp_distance_thresh: float = 500.0  # I think this is feet?
+    end_time: float = 60 * 60 * 24  # Default 24 hours
+    time_step: float = 1.0 / 50.0  # Default 50Hz
+
+
+def fix_jsb_latitude(lat_deg: float) -> float:
+    """
+    import tempfile
+    import jsbsim
+    import simplekml
+    from jsbsim_wrapper import build_wp_xml, fix_jsb_latitude
+    from helpers import fill_xml_template
+    def test_jsb_over_world():
+        frange = lambda start, stop, step: [(x * step) for x in range(int(start / step), int(stop / step), 1)]
+        kml = simplekml.Kml()
+        lat_diffs = []
+        lon_diffs = []
+        idx = 0
+        for requested_lat in frange(-90, 90, 5):
+            for requested_lon in frange(0, 1, 1):
+                jsbfdm = jsbsim.FGFDMExec(None)  # Use JSBSim default aircraft data
+                jsbfdm.set_debug_level(0)
+                init_script_xml = fill_xml_template('jsbsim_wrapper/xml_templates/jsb_initialize_vehicle.xml', lat_deg=fix_jsb_latitude(requested_lat), lon_deg=requested_lon, alt_m=1000)
+                wp_xml = build_wp_xml([], 100)
+                with tempfile.NamedTemporaryFile('w', suffix='.xml') as initialize_fp:
+                    initialize_fp.write(init_script_xml)
+                    initialize_fp.flush()
+                    veh_script_xml = fill_xml_template(
+                        'jsbsim_wrapper/xml_templates/jsb_vehicle_script.xml',
+                        init_script_path=initialize_fp.name,
+                        vehicle='c310',
+                        wp_xml=wp_xml,
+                        end_time=5000,
+                        time_step=(1/50),
+                    )
+                    with tempfile.NamedTemporaryFile('w', suffix='.xml') as script_fp:
+                        script_fp.write(veh_script_xml)
+                        script_fp.flush()
+                        jsbfdm.load_script(script_fp.name)
+                jsbfdm.run_ic()  # setup initial conditions
+                jsbfdm.run()
+                actual_lat = jsbfdm.get_property_value('position/lat-geod-deg')
+                actual_lon = jsbfdm.get_property_value('position/long-gc-deg')
+
+                # KML points are lon,lat,hgt
+                from_loc = [requested_lon, requested_lat, 1000]
+                to_loc = [actual_lon, actual_lat, 1000]
+
+                kml.newpoint(name=f'{idx}a', coords=[from_loc], altitudemode=simplekml.AltitudeMode.clamptoground)
+                kml.newpoint(name=f'{idx}b', coords=[to_loc], altitudemode=simplekml.AltitudeMode.clamptoground)
+                kml.newlinestring(name=str(idx), coords=[from_loc, to_loc], altitudemode=simplekml.AltitudeMode.clamptoground)
+
+                lat_diffs.append(requested_lat - actual_lat)
+                lon_diffs.append(requested_lon - actual_lon)
+                print(f'{requested_lat} {actual_lat:.6f}')
+                idx += 1
+        print(f'min: {min(lat_diffs)} {min(lon_diffs)}')
+        print(f'max: {max(lat_diffs)} {max(lon_diffs)}')
+        print(f'avg: {sum(lat_diffs)/len(lat_diffs)} {sum(lon_diffs)/len(lat_diffs)}')
+        kml.save('./JSB_geo_diff.kml', format=False)
+    """
+    # These constants fit to the error sort of well. Still 20m error at worst case
+    a = -0.192465694889549
+    b = 2.00002985781682
+    fixed_lat_deg = lat_deg + (a * math.sin(math.radians(lat_deg) * b))
+    return fixed_lat_deg
+
+
+def build_wp_xml(wp_list: List[Waypoint], wp_distance_thresh: float) -> str:
+    wp_xml = ''
+    for wp_idx, waypoint_data in enumerate(wp_list, start=1):
+        prev_wp_idx = len(wp_list) if wp_idx == 1 else wp_idx - 1
+        conditions = f'''
+<condition logic="AND">
+    ap/active-waypoint eq {prev_wp_idx}
+    guidance/wp-distance lt {wp_distance_thresh}
+</condition>
+'''
+        if wp_idx == 1:
+            # Add in the first condition: if active waypoint is 0 (not set), start going to 1
+            conditions = f'''
+<condition logic="OR">
+    ap/active-waypoint eq 0
+    {conditions}
+</condition>
+'''
+        wp_xml += fill_xml_template(
+            'jsbsim_wrapper/xml_templates/jsb_single_wp.xml',
+            wp_idx=wp_idx,
+            conditions=conditions,
+            lat_rad=math.radians(waypoint_data.lat_deg), lon_rad=math.radians(waypoint_data.lon_deg),
+            alt_ft=m_to_ft(waypoint_data.alt_m),
+        )
+    return wp_xml
+
+
+def build_initialize_xml(jsb_config: JsbConfig) -> str:
+    # Take the starting location from the last waypoint
+    start_lat_deg, start_lon_deg, start_alt_m = jsb_config.waypoints[-1]
+    init_script_xml = fill_xml_template(
+        'jsbsim_wrapper/xml_templates/jsb_initialize_vehicle.xml', lat_deg=start_lat_deg, lon_deg=start_lon_deg, alt_m=start_alt_m
+    )
+    return init_script_xml
+
+
+def build_vehicle_xml(jsb_config: JsbConfig, init_script_path: str) -> str:
+    wp_xml = build_wp_xml(jsb_config.waypoints, jsb_config.wp_distance_thresh)
+
+    outputs_config_xml = ''
+    for fg_output in jsb_config.flightgear_outputs:
+        output_config_xml = fill_xml_template(
+            'jsbsim_wrapper/xml_templates/jsb_output_config_flightgear.xml',
+            flightgear_ip=fg_output.ip_addr,
+            flightgear_port=fg_output.port,
+            flightgear_version=fg_output.fg_version,
+            flightgear_rate=fg_output.rate_hz,
+        )
+        outputs_config_xml += output_config_xml
+
+    veh_script_xml = fill_xml_template(
+        'jsbsim_wrapper/xml_templates/jsb_vehicle_script.xml',
+        init_script_path=init_script_path,  # Vehicle script references the initialize script path
+        vehicle=jsb_config.vehicle_str,
+        wp_xml=wp_xml,
+        end_time=jsb_config.end_time,
+        time_step=jsb_config.time_step,
+        outputs_xml=outputs_config_xml,
+    )
+    return veh_script_xml
+
+
+def make_and_load_jsb_scripts(jsbfdm: jsbsim.FGFDMExec, jsb_config: JsbConfig):
+    # Create initialize script
+    initialize_fp = tempfile.NamedTemporaryFile('w', suffix='.xml')
+    init_script_xml = build_initialize_xml(jsb_config)
+    initialize_fp.write(init_script_xml)
+    initialize_fp.flush()
+
+    # Create vehicle script
+    veh_script_fp = tempfile.NamedTemporaryFile('w', suffix='.xml')
+    veh_script_xml = build_vehicle_xml(jsb_config, initialize_fp.name)
+    print(f'Full vehicle script @ {veh_script_fp.name}:\n{veh_script_xml}')
+    veh_script_fp.write(veh_script_xml)
+    veh_script_fp.flush()
+
+    # Tell JSB to load the vehicle script
+    jsbfdm.load_script(veh_script_fp.name)
+
+    # cleanup
+    initialize_fp.close()
+    veh_script_fp.close()
+
+
+def setup_jsbsim(jsb_config: JsbConfig) -> jsbsim.FGFDMExec:
+    # Before we do anything, go through and fix the waypoints to be correct :/
+    fixed_waypoints = []
+    for waypoint in jsb_config.waypoints:
+        fixed_wp = Waypoint(fix_jsb_latitude(waypoint.lat_deg), waypoint.lon_deg, waypoint.alt_m)
+        fixed_waypoints.append(fixed_wp)
+    jsb_config = jsb_config._replace(waypoints=fixed_waypoints)
+
+    jsbfdm = jsbsim.FGFDMExec(None)  # Use JSBSim default aircraft data
+    default_dbg_level = jsbfdm.get_debug_level()
+
+    jsbfdm.set_debug_level(0)  # initial output is too noisy
+    make_and_load_jsb_scripts(jsbfdm, jsb_config)
+    jsbfdm.run_ic()  # setup initial conditions
+
+    jsbfdm.set_debug_level(default_dbg_level)
+
+    return jsbfdm
---
+++ tests/test_integration_using_JSBSim.py
@@ -0,0 +1,70 @@
+import math
+
+from flightgear_python.fg_if import FDMConnection
+# from testing_common import supported_fdm_versions
+from jsbsim_wrapper.jsbsim_wrapper import FlightGearUdpOutput, JsbConfig, Waypoint, setup_jsbsim
+
+import pytest
+
+# TODO: JSBSim 1.1.11 doesn't support FDM v25
+#  Once we drop 3.6 we can fully test v25
+supported_fdm_versions = [24]
+
+
+def fdm_callback(fdm_data, event_pipe):
+    child_data = (fdm_data['lat_rad'], fdm_data['lon_rad'], fdm_data['alt_m'],)
+    print('Callback!', child_data)
+    event_pipe.child_send(child_data)
+
+
+@pytest.mark.parametrize('fdm_version', supported_fdm_versions)
+def test_jsbsim_integration(fdm_version, capsys):
+    fg_to_py_port = 5000 + fdm_version  # So that tests can run parallel
+    fdm_conn = FDMConnection(fdm_version=fdm_version)
+    fdm_conn.connect_rx('localhost', fg_to_py_port, fdm_callback)
+
+    update_rate = 60
+    jsb_time_step = 1 / update_rate
+    jsb_config = JsbConfig(
+        waypoints=[
+            Waypoint(46.667094, 7.827569, 2500),
+            Waypoint(46.646220, 7.649860, 3000),
+            Waypoint(46.765000, 7.626200, 3500),
+        ],
+        flightgear_outputs=[
+            FlightGearUdpOutput('localhost', fg_to_py_port, update_rate, fg_version=fdm_version),
+        ],
+        time_step=jsb_time_step,
+    )
+    fdm_conn.start()
+
+    jsbfdm = setup_jsbsim(jsb_config)
+
+    total_sim_steps = int(1e4)
+    pos_history = []
+    for sim_step_idx in range(total_sim_steps):
+        if not jsbfdm.run():
+            print(f'Test ended early {fdm_version}, {sim_step_idx}')
+            assert False
+
+        assert fdm_conn.rx_proc.is_alive()
+
+        if fdm_conn.event_pipe.parent_poll(timeout=jsb_time_step):
+            lat_rad, lon_rad, alt_m = fdm_conn.event_pipe.parent_recv()
+            lat_deg = math.degrees(lat_rad)
+            lon_deg = math.degrees(lon_rad)
+            pos_history.append((sim_step_idx, lat_deg, lon_deg, alt_m))
+
+    fdm_conn.stop()
+
+    # Make sure we got at least half of the updates
+    total_updates = len(pos_history)
+    assert total_updates > (total_sim_steps / 2)
+    sim_step_idx, lat_deg, lon_deg, alt_m = pos_history[-1]
+    # Check the end location
+    assert 46.6 < lat_deg < 46.8
+    assert 7.6 < lon_deg < 7.9
+    assert 2000 < alt_m < 4000
+
+    with capsys.disabled():
+        print(f'\nLast update (got {total_updates}/{total_sim_steps}) @ {sim_step_idx}: {lat_deg},{lon_deg},{alt_m}')
