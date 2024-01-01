9b949e0c71b208a22b2aaf97913524c273de11c0
Julianne Swinoga <julianneswinoga@gmail.com>
2024-01-01 13:46:35 -0500

    Add telnet integration tests

---
+++ tests/test_integration_FG_telnet.py
@@ -0,0 +1,44 @@
+import time
+
+import pytest
+
+from flightgear_python.fg_if import PropsConnection
+
+
+pytestmark = pytest.mark.fg_integration
+
+
+def test_telnet_list_props():
+    t_con = PropsConnection('localhost', 5500)
+    t_con.connect()
+
+    sun_prop_dict_immediate = t_con.list_props('/ephemeris/sun', recurse_limit=0)
+    assert len(sun_prop_dict_immediate['directories']) != 0
+    assert len(sun_prop_dict_immediate['properties']) != 0
+
+    sun_prop_dict_all = t_con.list_props('/ephemeris/sun', recurse_limit=None)
+    assert len(sun_prop_dict_all['directories']) == 0
+    assert len(sun_prop_dict_all['properties']) != 0
+
+
+def test_telnet_get_prop():
+    t_con = PropsConnection('localhost', 5500)
+    t_con.connect()
+
+    sim_time_1 = t_con.get_prop('/sim/time/steady-clock-sec')
+    time.sleep(1)
+    sim_time_2 = t_con.get_prop('/sim/time/steady-clock-sec')
+    assert sim_time_2 > sim_time_1
+
+
+def test_telnet_set_prop():
+    t_con = PropsConnection('localhost', 5500)
+    t_con.connect()
+
+    t_con.set_prop('/controls/flight/rudder', 0.5)
+    aileron_value = t_con.get_prop('/controls/flight/rudder')
+    assert aileron_value == 0.5
+
+    t_con.set_prop('/controls/flight/rudder', -0.5)
+    aileron_value = t_con.get_prop('/controls/flight/rudder')
+    assert aileron_value == -0.5
