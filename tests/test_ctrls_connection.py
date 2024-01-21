d9f34308720a60f8631ea6b07825e019a8f5c984
Julianne Swinoga <julianneswinoga@gmail.com>
2024-01-21 17:36:55 -0500

    Fix FGConnection RX timeout, add RX timeout tests

--- tests/test_ctrls_connection.py
+++ tests/test_ctrls_connection.py
@@ -23,3 +23,12 @@ def test_ctrls_bad_port(mocker, ctrls_version):
     with pytest.raises(FGConnectionError):
         # Should fail with [Errno 13] Permission denied
         ctrls_c.connect_rx('localhost', 1, lambda data, pipe: data)
+
+
+@pytest.mark.timeout(3)  # If this fails don't wait a long time
+@pytest.mark.parametrize('ctrls_version', supported_ctrls_versions)
+def test_ctrls_rx_timeout(mocker, ctrls_version):
+    ctrls_c = CtrlsConnection(ctrls_version, rx_timeout_s=0.1)
+    ctrls_c.connect_rx('localhost', 9999, lambda data, pipe: data)
+    with pytest.raises(FGConnectionError, match='[Tt]imeout'):
+        ctrls_c._fg_packet_roundtrip()
--- tests/test_gui_connection.py
+++ tests/test_gui_connection.py
@@ -23,3 +23,12 @@ def test_gui_bad_port(mocker, gui_version):
     with pytest.raises(FGConnectionError):
         # Should fail with [Errno 13] Permission denied
         gui_c.connect_rx('localhost', 1, lambda data, pipe: data)
+
+
+@pytest.mark.timeout(3)  # If this fails don't wait a long time
+@pytest.mark.parametrize('gui_version', supported_gui_versions)
+def test_gui_rx_timeout(mocker, gui_version):
+    gui_c = GuiConnection(gui_version, rx_timeout_s=0.1)
+    gui_c.connect_rx('localhost', 9999, lambda data, pipe: data)
+    with pytest.raises(FGConnectionError, match='[Tt]imeout'):
+        gui_c._fg_packet_roundtrip()
